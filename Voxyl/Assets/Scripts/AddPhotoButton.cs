73c710a825c48a1b59887e4cb77b70fd6f3a0290
Alex Bertrand <alex.bertrand13@gmail.com>
2016-11-26 01:32:04 -0800

    Can now create cubes on the fly

---
+++ Voxyl/Assets/Scripts/AddPhotoButton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class AddPhotoButton : MonoBehaviour {
+
+	[SerializeField] private VRInteractiveItem m_InteractiveItem;
+	public Transform m_Cam;
+
+
+	public Transform imgTemplate;
+
+	private void OnEnable ()
+	{
+		m_InteractiveItem.OnClick += HandleClick;
+	}
+
+	private void OnDisable ()
+	{
+		m_InteractiveItem.OnClick -= HandleClick;
+	}
+
+	private void HandleClick() {
+		// create cube and spawn
+		print("Clicked");
+
+		/*GameObject newCube = new GameObject("MyCube");
+		newCube.AddComponent<MeshFilter>();
+		newCube.AddComponent<BoxCollider>();
+		newCube.AddComponent<MeshRenderer>();
+		newCube.AddComponent<DragAndDroppable>();*/
+		
+		// create using prefab
+		// DragAndDroppable img = Instantiate(Resources.Load("Prefabs/Image")) as DragAndDroppable;
+		// img.m_Cam = m_Cam;
+		// DragAndDroppable img = Resources.Load("Assets/Prefabs/Image") as DragAndDroppable;
+		// img.m_Cam = Camera.main.transform;
+		// print(img);
+		// Instantiate(img);
+		
+
+		Instantiate(imgTemplate, new Vector3(3, 3, 3), Quaternion.identity);
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Voxyl/Assets/Scripts/DragAndDroppable.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils; // add this to deal with VR inputs
+
+public class DragAndDroppable : MonoBehaviour {
+
+	private bool selected;
+	private float distance;
+	private float relativeSize;
+
+   	// For these serialize field things, I think you also have to make them a thing in the Scene editor?
+	public  VRInteractiveItem m_InteractiveItem;       // The interactive item for where the user should click to load the level.
+	// public Transform m_Cam;
+	private Transform m_Cam;
+	public VRInput m_VrInput;
+
+	private bool m_GazeOver;
+
+	private void OnEnable ()
+	{
+		m_InteractiveItem.OnOver += HandleOver;
+		m_InteractiveItem.OnOut += HandleOut;
+		m_InteractiveItem.OnClick += HandleClick;
+
+		m_VrInput.OnSwipe += HandleSwipe;
+
+		m_Cam = Camera.main.transform;
+
+		distance = Vector3.Distance(transform.position, m_Cam.position);
+		relativeSize = 1f;
+	}
+
+	private void OnDisable ()
+	{
+		m_InteractiveItem.OnOver -= HandleOver;
+		m_InteractiveItem.OnOut -= HandleOut;
+		m_InteractiveItem.OnClick -= HandleClick;
+		
+		m_VrInput.OnSwipe -= HandleSwipe;
+		// m_SelectionRadial.OnSelectionComplete -= HandleSelectionComplete;
+	}
+
+	private void HandleOver() {
+		m_GazeOver = true;
+		// Debug.Log("Looking at it!");
+	}
+
+	private void HandleOut() {
+		m_GazeOver = false;	
+		// print("Not looking at it!");
+	}
+
+	private void HandleClick() {
+		selected = !selected;
+		print("Object " + (selected ? "selected" : "un-selected"));
+		// print(transform.position.x);
+		// print(
+	}
+
+	private void HandleSwipe(VRInput.SwipeDirection swipeDirection) {
+		if(selected) {
+			switch(swipeDirection) {
+				case VRInput.SwipeDirection.UP:
+					relativeSize += .3f;
+					break;
+
+				case VRInput.SwipeDirection.DOWN:
+					relativeSize -= .3f;
+					break;
+				
+				case VRInput.SwipeDirection.LEFT:
+					distance += 1f;
+					break;
+
+				case VRInput.SwipeDirection.RIGHT:
+					distance -= 1f;
+					break;
+			}
+			print("Size is now at " + relativeSize);
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (selected) {
+			// follow gaze
+			transform.position = m_Cam.position + m_Cam.forward * distance;
+			transform.localScale = new Vector3(relativeSize, relativeSize, relativeSize);
+		}
+	}
+}
