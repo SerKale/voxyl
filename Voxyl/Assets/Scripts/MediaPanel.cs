ecf6feac095b49251ff486b0a540b367ad5751e0
Alex Bertrand <alex.bertrand13@gmail.com>
2016-11-26 14:26:29 -0800

    Pre API update backup

---
+++ Voxyl/Assets/Scripts/MediaPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MediaPanel : MonoBehaviour {
+
+	public Transform media;
+
+	public string url = "TempAssets/chandra.jpg";
+
+	// Use this for initialization
+	IEnumerator Start () {
+		renderer.material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+		while(true) {
+			WWW www = new WWW(url);
+			yield return www;
+			www.LoadImageIntoTexture(renderer.material.mainTexture);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
