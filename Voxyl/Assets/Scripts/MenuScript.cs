5cc1765b82acbdeed55e06960b1f6b7e4fc0268b
Josh Cohen <jhcohen@stanford.edu>
2016-11-27 15:13:40 -0800

    Added basic clickable buttons, for use with main menu

---
+++ Voxyl/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	public void ChangeScene(string sceneName) {
+		Application.LoadLevel (sceneName);	
+ 	}
+}
