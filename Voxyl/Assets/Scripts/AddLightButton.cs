b7f31425983e1847c3c3fdac3c11b122969cc1e9
Hana Lee <leehana@stanford.edu>
2016-12-05 23:40:43 -0800

    Can drag and drop lights and change intensity by swiping up or down

---
+++ Voxyl/Assets/Scripts/AddLightButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class AddLightButton : MonoBehaviour
+{
+
+    [SerializeField]
+    private VRInteractiveItem m_InteractiveItem;
+    public Transform m_Cam;
+
+
+    public Transform imgTemplate;
+
+    private void OnEnable()
+    {
+        m_InteractiveItem.OnClick += HandleClick;
+    }
+
+    private void OnDisable()
+    {
+        m_InteractiveItem.OnClick -= HandleClick;
+    }
+
+    private void HandleClick()
+    {
+        Transform obj = (Transform)Instantiate(imgTemplate, new Vector3(3, 3, 3), Quaternion.identity);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
