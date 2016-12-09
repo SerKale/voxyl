3dc75a1badf31cd217605b3f8e0d5da37a248105
Josh Cohen <jhcohen@stanford.edu>
2016-12-08 23:04:22 -0800

    Updated main menu to contain share, save, and exit functionality. Also trash can icon

---
+++ Voxyl/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class ExitScript : MonoBehaviour {
+
+	public string m_NextScene;
+	public VRInteractiveItem m_InteractiveItem;
+
+	private void OnEnable ()
+	{
+		m_InteractiveItem.OnClick += HandleClick;
+	}
+
+	private void OnDisable ()
+	{
+		m_InteractiveItem.OnClick -= HandleClick;
+	}
+
+	private void HandleClick() {
+		print ("Clicked!");
+		Application.LoadLevel (m_NextScene);
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
