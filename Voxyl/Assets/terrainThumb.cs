3076d5b25ffdc5868c07f990bbc8c1fbc64b39de
Alex Bertrand <alex.bertrand13@gmail.com>
2016-12-09 10:14:01 -0800

    Temporary commit while we unfuck things from merge
    FML.
    But I think we're okay.
    The kids are alright.
    JK they're screaming inside.
    Like me.
    
    RIP.

---
+++ Voxyl/Assets/terrainThumb.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class terrainThumb : MonoBehaviour {
+
+	private VRInteractiveItem m_InteractiveItem;
+	public GameObject terrain;
+	public Transform parent;
+
+	private void OnEnable() {
+		m_InteractiveItem.OnClick += HandleClick;
+	}
+
+	private void OnDisable() {
+		m_InteractiveItem.OnClick -= HandleClick;
+	}	
+
+	private void HandleClick() {	
+		foreach (Transform child in parent) {
+			child.gameObject.SetActive(false);
+		}
+
+		if (terrain) { terrain.SetActive(true); }
+
+		transform.parent.gameObject.SetActive(false);
+	}
+
+	// Use this for initialization
+	void Awake () {	
+		m_InteractiveItem = GetComponent<VRInteractiveItem>();
+	}
+}
