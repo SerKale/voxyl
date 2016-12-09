f51927bf1ec5e644a9ae65e600c8a3204c329f78
Kale the kale <kaley@kunzelman.net>
2016-12-09 02:11:32 -0800

    Added terrain button and panel

---
+++ Voxyl/Assets/AddTerrainButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class AddTerrainButton : MonoBehaviour {
+
+	public GameObject m_terrainPanel;
+	private VRInteractiveItem m_InteractiveItem;
+
+	private void OnEnable ()
+	{
+		m_InteractiveItem.OnClick += HandleClick;
+	}
+
+	private void OnDisable ()
+	{
+		m_InteractiveItem.OnClick -= HandleClick;
+	}
+
+	private void HandleClick() {
+		m_terrainPanel.SetActive(true);
+	}
+
+	void Awake () {
+		m_InteractiveItem = GetComponent<VRInteractiveItem>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		m_terrainPanel.SetActive(false);	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
