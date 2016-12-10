375180b80daa1a581e8ac7b75ec6fc61693039fc
Alex Bertrand <alex.bertrand13@gmail.com>
2016-12-09 18:27:55 -0800

    Added kale's test scene

---
+++ Voxyl/Assets/SceneThumb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+using UnityEngine.SceneManagement;
+
+public class SceneThumb : MonoBehaviour {
+
+	private VRInteractiveItem m_InteractiveItem;
+	public string toLoad;
+
+	private void OnEnable() {
+		m_InteractiveItem.OnClick += HandleClick;
+	}
+
+	private void OnDisable() {
+		m_InteractiveItem.OnClick -= HandleClick;
+	}	
+
+	private void HandleClick() {	
+		SceneManager.LoadScene(toLoad, LoadSceneMode.Single);
+	}
+
+	// Use this for initialization
+	void Awake () {	
+		m_InteractiveItem = GetComponent<VRInteractiveItem>();
+	}
+}
